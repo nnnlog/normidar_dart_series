6c1df4a57e4f20825d2745908bc2f3a426b12c97
normidar <36730656+normidar@users.noreply.github.com>
2024-06-15 07:42:44 +0000

    update riverpod_helper

--- apps/riverpod_helper/CHANGELOG.md
+++ apps/riverpod_helper/CHANGELOG.md
@@ -1,3 +1,8 @@
+## [0.0.2] - 2024-06-15
+
+* first release
+
 ## 0.0.1
 
 * first release
+
--- apps/riverpod_helper/lib/riverpod_helper.dart
+++ apps/riverpod_helper/lib/riverpod_helper.dart
@@ -1,7 +1,7 @@
-library riverpod_helper;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+// run this to reset your file:  dart run build_runner build
+// or use flutter:               flutter packages pub run build_runner build
+// remenber to format this file, you can use: dart format
+// publish your package hint: dart pub publish --dry-run
+// if you want to update your packages on power: dart pub upgrade --major-versions
+export 'package:riverpod_helper/src/prefs.dart';
+export 'package:riverpod_helper/src/riverpod_error_view.dart';
