32c4e054061764d99241033245e328d64e21018a
normidar <36730656+normidar@users.noreply.github.com>
2024-07-03 02:10:33 +0000

    delet fm example

--- apps/firebase_manager/example/android/app/src/main/kotlin/com/example/app_exm/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.normidar.firebase_manager_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- apps/firebase_manager/example/lib/access/firestore.dart
+++
@@ -1,9 +0,0 @@
-import 'package:firebase_manager/firebase_manager.dart';
-
-/// you create a random id doc
-/// and then you can get the id
-void toGetADocIdAlthoughItDoesNotExist() async {
-  final mainColl = fs.db.fireStore.getMainColl();
-  final doc = mainColl.getRandomNameDoc();
-  doc.getID();
-}
