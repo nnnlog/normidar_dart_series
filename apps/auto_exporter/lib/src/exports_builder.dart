3eb015af28f0d8e4eb1ce5b91518f8590795a2c8
nlog (solrin) <sorisem4106@naver.com>
2024-09-20 18:29:54 +0900

    fix: typo

--- apps/auto_exporter/lib/src/exports_builder.dart
+++ apps/auto_exporter/lib/src/exports_builder.dart
@@ -34,7 +34,7 @@ class ExportsBuilder implements Builder {
     final head = [
       "// run this to reset your file:  dart run build_runner build",
       "// or use flutter:               flutter packages pub run build_runner build",
-      "// remenber to format this file, you can use: dart format",
+      "// remember to format this file, you can use: dart format",
       "// publish your package hint: dart pub publish --dry-run",
       "// if you want to update your packages on power: dart pub upgrade --major-versions",
     ];
